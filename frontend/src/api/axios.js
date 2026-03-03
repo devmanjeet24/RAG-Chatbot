910a741f73d80c1be0ac7fc9bb08973877508360
manjeet98 <manjeet9871124@gmail.com>
2026-03-03 11:26:25 +0530

    dfdfd

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "process.env.baseURL/api",
 });
 
 API.interceptors.request.use((req) => {
